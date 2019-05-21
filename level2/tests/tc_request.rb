9ab81c21a6d32f56e6eb5057ed4a5a3ded980609
TinQk <potie.quentin@gmail.com>
2019-05-21 02:18:17 +0200

    lvl2: add basic model tests

---
+++ level2/tests/tc_request.rb
@@ -0,0 +1,36 @@
+require 'test/unit'
+
+class TestRequest < Test::Unit::TestCase
+  def setup
+    @field1 = Field.new(1, 'mathematics')
+    @field2 = Field.new(2, 'history')
+    @level = Level.new(1, 'sixth', 'middle')
+    @skill1 = {field: @field1, level: @level}
+    @skill2 = {field: @field2, level: @level}
+    @teacher = Teacher.new(1, 'Quentin', 'Potie', [@skill1, @skill2])
+    @request = Request.new(1, 'John', 'Doe', @level, @field1, @teacher)
+  end
+
+  def test_new
+    assert_equal(1, @request.id)
+    assert_equal('John', @request.client_firstname)
+    assert_equal('Doe', @request.client_lastname)
+    assert_equal(@level, @request.level)
+    assert_equal(@field1, @request.field)
+    assert_equal(@teacher, @request.selected_teacher)
+  end
+
+  def test_to_json
+    assert_equal(
+      @request.to_json,
+      {
+        "id": 1,
+        "client_firstname": "John",
+        "client_lastname": "Doe",
+        "level": 1,
+        "field": 1,
+        "selected_teacher": 1
+      }
+    )
+  end
+end
---
+++ level2/tests/tc_teacher.rb
@@ -0,0 +1,41 @@
+require 'test/unit'
+
+class TestTeacher < Test::Unit::TestCase
+  def setup
+    @field1 = Field.new(1, 'mathematics')
+    @field2 = Field.new(2, 'history')
+    @level = Level.new(1, 'sixth', 'middle')
+    @skill1 = {field: @field1, level: @level}
+    @skill2 = {field: @field2, level: @level}
+    @teacher = Teacher.new(1, 'Quentin', 'Potie', [@skill1, @skill2])
+  end
+
+  def test_new
+    assert_equal(1, @teacher.id)
+    assert_equal('Quentin', @teacher.firstname)
+    assert_equal('Potie', @teacher.lastname)
+    assert_send([@teacher.skills, :include?, @skill1])
+    assert_send([@teacher.skills, :include?, @skill2])
+  end
+
+  def test_to_json
+    assert_equal(
+      @teacher.to_json,
+      {
+        "id": 1,
+        "firstname": "Quentin",
+        "lastname": "Potie",
+        "skills": [
+          {
+            field: 1,
+            level: 1
+          },
+          {
+            field: 2,
+            level: 1
+          }
+        ]
+      }
+    )
+  end
+end
